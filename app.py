42316466089992ef254638ee3eef00f743048ca8
Joao Reis <joaovictorcruzereis@gmail.com>
2021-05-11 23:47:05 -0300

    [ROUTE] Read/Upload/Download de arquivos na AWS s3

--- app.py
+++ app.py
@@ -1,54 +1,50 @@
 import os
 from markupsafe import escape
 from werkzeug.utils import secure_filename
-from flask import Flask, flash, request, redirect, url_for, render_template, send_from_directory
-
-
-UPLOAD_FOLDER = r'C:\Users\jd\Documents\projetos\crud\backend-flask\uploads'
-ALLOWED_EXTENSIONS = {'txt', 'png', 'jpeg', 'jpg', 'mp3', 'mp4'}
+from flask import Flask, flash, request, redirect, url_for, render_template, send_from_directory, send_file
+from s3_demo import list_files, download_file, upload_file
 
 app = Flask(__name__)
-app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+
+#app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+#UPLOAD_FOLDER = "uploads"
+BUCKET = "ecomidias-s3"
+ALLOWED_EXTENSIONS = {'txt', 'png', 'jpeg', 'jpg', 'mp3', 'mp4'}
 
 
 def allowed_file(filename):
     return (('.' in filename) and (filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS))
 
 
-@app.route('/', methods=['GET', 'POST'])
-def upload_file():
+@app.route('/')
+def entry_point():
+    return 'Hello World!'
+
+@app.route("/storage")
+def storage():
+    contents = list_files(BUCKET)
+    return render_template('index.html', contents=contents)
+
+@app.route('/upload', methods=['POST'])
+def upload():
     if request.method == 'POST':
-        # Check if the request has the file
-        if 'file' not in request.files:
-            flash('No file part')
-            return redirect(request.url)
-        file = request.files['file']
-        # If user does not select a file, browser also
-        # submit an empty without filename
-        if file.filename == '':
-            flash('No selected file')
-            return redirect(request.url)
-        if file and allowed_file(file.filename):
-            filename = secure_filename(file.filename)
-            file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
-            return redirect(url_for('uploaded_file', filename=filename))
+        f = request.files['file']
+        #f.save(os.path.join(UPLOAD_FOLDER, f.filename))
+        f.save(f.filename)
+        #upload_file(f'uploads/{f.filename}', BUCKET)
+        upload_file(f'{f.filename}', BUCKET)
 
-    return '''
-    <!doctype html>
-    <title>Upload new File</title>
-    <h1>Upload new File</h1>
-    <form method=post enctype=multipart/form-data>
-      <input type=file name=file>
-      <input type=submit value=Upload>
-    </form>
-    '''
+        return redirect("/storage")
 
-@app.route('/uploads/<filename>')
-def uploaded_file(filename):
-    return send_from_directory(app.config['UPLOAD_FOLDER'],
-                               filename)
+@app.route("/download/<filename>", methods=['GET'])
+def download(filename):
+    if request.method == 'GET':
+        output = download_file(filename, BUCKET)
+
+        return send_file(output, as_attachment=True)
 
 if __name__ == '__main__':
     app.debug = True
     app.env = 'development'
-    app.run(host="0.0.0.0", port=80)
+    #app.run(host="0.0.0.0", port=80)
+    app.run()
---
+++ config.py
@@ -0,0 +1,1 @@
+s3_bucket = 'ecomidias-s3'
